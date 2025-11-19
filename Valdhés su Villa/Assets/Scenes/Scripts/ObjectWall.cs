2fdd555bddb696bd6aa342f34881ab0770869ec8
oppq <oqqppqoppqppq@gmail.com>
2025-11-19 23:31:34 +0800

    updatecamerav0.1
    Адаптация основного кода камеры

---
+++ Valdhés su Villa/Assets/Scenes/Scripts/ObjectWall.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallObjects : MonoBehaviour
+{
+    public WallController wallController;
+    public List<Transform> objectsForWall0 = new List<Transform>();
+    public List<Transform> objectsForWall1 = new List<Transform>();
+    public List<Transform> objectsForWall2 = new List<Transform>();
+    public List<Transform> objectsForWall3 = new List<Transform>();
+
+    void Start()
+
+    {
+        AddObjectsToWalls();
+    }
+
+    void AddObjectsToWalls()
+    {
+        foreach (Transform obj in objectsForWall0)
+            WallController.AddObject(obj, 0);
+        foreach (Transform obj in objectsForWall1)
+            WallController.AddObject(obj, 1);
+        foreach (Transform obj in objectsForWall2)
+            WallController.AddObject(obj, 2);
+        foreach (Transform obj in objectsForWall3)
+            WallController.AddObject(obj, 3);
+    }
+
+    public void AddNewObject(Transform obj, int wallIndex)
+    {
+        switch (wallIndex)
+        {
+            case 0: objectsForWall0.Add(obj); break;
+            case 1: objectsForWall1.Add(obj); break;
+            case 2: objectsForWall2.Add(obj); break;
+            case 3: objectsForWall3.Add(obj); break;
+        }
+        wallController.AddObject(obj, wallIndex);
+    }
+}
---
+++ Valdhés su Villa/Assets/Scenes/Scripts/WallController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using UnityEngine;
+
+public class WallController : MonoBehaviour
+
+{
+    public Transform[] walls = new Transform[4]; // Создание 4-х стен
+
+    public float moveTime = 0.5f;
+
+    private int currentView = 0;
+    private bool Move = false;
+    private Vector3 hiddenPos = new Vector3(0, 100f, 0);
+
+    private int[,] views = 
+    {
+        {0, 1, 2, 3},
+        {0, 3, 1, 2},
+        {2, 3, 0, 1},
+        {2, 1, 0, 3}
+    };
+
+    void Start()
+
+    {
+        Walls(0);
+    }
+
+    void Update()
+
+    {
+        if (Move) return;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) Walls(-1); // <-- binds
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) Walls(1); // -->
+        else if (Input.GetKeyDown(KeyCode.Alpha1)) Walls(0); // 1
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) Walls(1); // 2
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) Walls(2); // 3
+        else if (Input.GetKeyDown(KeyCode.Alpha4)) Walls(3); // 4
+    }
+
+    void View(int direction)
+
+    {
+
+        int newView = (currentView + direction + 4) % 4; // Всего 4 вида
+        StartCoroutine(MoveWalls(newView));
+
+    }
+
+    void Walls(int view)
+
+    {
+
+        if (view < 0 || view > 3 || Move) return;
+        StartCoroutine(MoveWalls(view));
+    }
+
+    IEnumerator MoveWalls(int newView)
+
+    {
+        Move = true;
+
+        int hide1 = views[currentView, 2];
+        int hide2 = views[currentView, 3];
+        int show1 = views[newView, 0];
+        int show2 = views[newView, 1];
+
+        float time = 0f;
+
+        while (time < moveTime)
+        {
+            float t = time / moveTime;
+
+            if (walls[hide1] != null)
+                walls[hide1].position = Vector3.Lerp(walls[hide1].position, hiddenPos, t);
+            if (walls[hide2] != null)
+                walls[hide2].position = Vector3.Lerp(walls[hide2].position, hiddenPos, t);
+            if (walls[show1] != null)
+                walls[show1].position = Vector3.Lerp(walls[show1].position, Vector3.zero, t);
+            if (walls[show2] != null)
+                walls[show2].position = Vector3.Lerp(walls[show2].position, Vector3.zero, t);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        currentView = newView;
+        Move = false;
+    }
+}
